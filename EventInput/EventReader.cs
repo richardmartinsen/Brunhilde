987870a4ac27080b96f9cd5dbe557dac13f3cc55
tobirek <tbre@live.no>
2017-02-03 01:07:27 +0100

    Commit før pull

---
+++ EventInput/EventReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs.Processor;
+
+namespace Brunhilde.EventInput
+{
+    public sealed class EventReader
+    {
+        private IEventProcessor _eventProcessor;
+        private PartitionContext _partitionContext;
+
+        public EventReader(PartitionContext partitionContext)
+        {
+            _eventProcessor = new SimpleEventProcessor();
+        }
+    }
+}
